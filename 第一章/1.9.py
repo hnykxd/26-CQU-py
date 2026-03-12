a2fe62b7d04471d2ccf0fc115dd1445cd015809d
hnykxd <120177280+hnykxd@users.noreply.github.com>
2026-03-12 12:53:35 +0800

    Update 1.9.py

--- 第一章/1.9.py
+++ 第一章/1.9.py
@@ -2,5 +2,4 @@ import math
 r = eval(input("请输入圆的半径:"))
 fArea = math.pi*r*r
 fPerimeter = 2*r*math.pi
-print("周长:{:.2f},面积:{:.3f}".format(fPerimeter,fArea))
-
+print(f"周长:{fPerimeter:.2f},面积:{fArea:.3f}")

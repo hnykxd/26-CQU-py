c9446ae0b99fa8e907e8dd8ebbed22dc1559feba
hnykxd <120177280+hnykxd@users.noreply.github.com>
2026-04-12 19:25:00 +0800

    1

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,9 +1,16 @@
-def Fibonacci(num, n):
+def Fibonacci(num, n): #求前n个数
     for i in range (2,n):
         numnew = num[i-1] +num[i-2]
         num.append(numnew)
 
     return num[n-1]
+
+def Fibonacci2(n): #仅仅求最后一个数
+    a = 1
+    b = 1
+    for _ in range (n):
+        a, b = b, a+b
+    return a
 
 num = [1,1]
 n = 3
